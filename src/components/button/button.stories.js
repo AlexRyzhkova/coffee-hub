40ec5cae2426dfec1b2af8cc6039d5ccac7bb823
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-07-28 16:29:20 +0200

    Fixed alert

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -4,24 +4,24 @@ import "./button.css";
 export default { title: "Button" };
 
 function createButton(buttonText) {
-const button = document.createElement("button");
- button.className = "btn";
- const text = document.createTextNode(buttonText); 
- button.append(text); 
- 
- button.addEventListener( "click" , () => {alert("Hallo");
-})
+  const button = document.createElement("button");
+  button.className = "btn";
+  const text = document.createTextNode(buttonText);
+  button.append(text);
 
-return button;
+  button.addEventListener("click", () => {
+    alert("Hallo");
+  });
+
+  return button;
 }
 
 export const addToCart = () => {
+  const button = createButton("Add to cart");
 
-   const button = createButton("Add to cart");
+  // const button = document.createElement("button");
+  // button.className ="btn";
+  // button.innerText = "Add to cart";
 
-    // const button = document.createElement("button");
-    // button.className ="btn";
-    // button.innerText = "Add to cart";
-
-return button;
+  return button;
 };
