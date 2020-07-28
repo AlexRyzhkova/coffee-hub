5f9eea6535ce45d45945fe3a9e919a848907cd3a
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-07-28 15:12:35 +0200

    Add createButton function

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -3,10 +3,25 @@ import "./button.css";
 
 export default { title: "Button" };
 
+function createButton(buttonText) {
+const button = document.createElement("button");
+ button.className = "btn";
+ const text = document.createTextNode(buttonText); 
+ button.append(text); 
+ 
+ button.addEventListener( "click" , () => {alert("Hallo");
+})
+
+return button;
+}
+
 export const addToCart = () => {
-    const button = document.createElement("button");
-    button.className ="btn";
-    button.innerText = "Add to cart";
+
+   const button = createButton("Add to cart");
+
+    // const button = document.createElement("button");
+    // button.className ="btn";
+    // button.innerText = "Add to cart";
 
 return button;
 };
