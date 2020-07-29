ae4d00af4e5a6de798d67fbddcc993ef7800193f
AlexRyzhkova <alexandra.ryzhkova@gmail.com>
2020-07-29 22:33:12 +0200

    Add details page

---
+++ src/pages/details/details.js
@@ -0,0 +1,55 @@
+import { createButton } from "../../components/button/button";
+import { createElement } from "../../utils/element";
+import { createQuantitySelector } from "../../components/quantitySelector/quanttitySelector";
+import macchiatoSrc from "../../assets/macchiato.svg";
+
+const createForm = () => {
+  const coffeeName = createElement("div", { innerText: "Machhiato" });
+  const price = createElement("div", { innerText: "$2.90" });
+  coffeeName.append(price);
+
+  const coffeeSize = createElement("label", {});
+  const size = createElement("input");
+  coffeeSize.append(size);
+
+  const coffeSugar = createElement("label", {});
+  const sugar = createElement("input", {});
+  coffeSugar.append(sugar);
+
+  const quantitySelector = createQuantitySelector();
+
+  const button = createButton("Add to cart");
+
+  const form = createElement("main", {});
+
+  form.append(coffeeName);
+  form.append(quantitySelector);
+  form.append(coffeeSize);
+  form.append(coffeSugar);
+  form.append(button);
+
+  return form;
+};
+
+const createHeader = () => {
+  const header = createElement("header", { className: "details__header" });
+  const titel = createElement("h2", { innerText: "Macchiato" });
+  const img = createElement("img", { src: macchiatoSrc, alt: "Macchiato cup" });
+  header.append(titel);
+  header.append(img);
+
+  return header;
+};
+
+export const createDetailsPage = () => {
+  const main = createElement("main", { className: "details" });
+  const header = createHeader();
+  const form = createForm();
+  main.append(header);
+  main.append(form);
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+  return main;
+};
--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,15 +1,13 @@
-import { createButton } from "../../components/button/button";
+// import { createButton } from "../../components/button/button";
+// import { createElement } from "../../utils/element";
+// import { createQuantitySelector } from "../../components/quantitySelector/quanttitySelector";
+import { createDetailsPage } from "./details";
 
 export default {
   title: "Pages/Details",
 };
 
 export const basic = () => {
-  const main = document.createElement("main");
-  // const quantitySelector = createQuantitySelector();
-  // main.append(quantitySelector);
-  const button = createButton("Add to cart");
-  main.append(button);
-
-  return main;
+  const detailsPage = createDetailsPage();
+  return detailsPage;
 };
